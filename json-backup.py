5706e61b120d298439d33cec3c4a89009e5b90a1
Christopher Su <christophersu9@gmail.com>
2014-07-26 23:54:45 -0700

    stopping for the day

--- json-backup.py
+++ json-backup.py
@@ -1,9 +1,20 @@
 from quora import Quora, Activity
 import click
+import os
 
-quora = Quora()
+def sync_items(old, new):
+    for item in new:
+        if item.id 
 
 @click.command()
+@click.option('--path', '-p', help='specify a path to store the JSON files at')
 @click.argument('user')
-def sync(user):
-    print quora.get_activity(user)
+def sync(user, path):
+    if path is None:
+        path = os.getcwd()
+    quora = Quora()
+    activity = quora.get_activity(user)
+    
+
+if __name__ == '__main__':
+    sync()
