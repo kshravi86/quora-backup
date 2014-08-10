20d6f7e9d208f073cebd99539060a394b4f2595c
Christopher Su <christophersu9@gmail.com>
2014-08-10 16:01:58 -0700

    update pyquora

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='quorabackup',
-    version='0.1.6',
+    version='0.1.8',
     author='Christopher J. Su',
     author_email='christophersu9@gmail.com',
     packages=['quorabackup'],
@@ -15,7 +15,7 @@ setup(
         "click == 2.4",
         "feedparser == 5.1.3",
         "pymongo == 2.7.2",
-        "quora == 0.1.5",
+        "quora == 0.1.6",
         "requests == 2.3.0",
         "wsgiref == 0.1.2"
     ],
