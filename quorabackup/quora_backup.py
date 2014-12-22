346fb5a280cab4ae8772ca6b3ae982464d01cac3
Christopher Su <christophersu9@gmail.com>
2014-12-22 08:45:19 +0100

    update for pyquora

--- quorabackup/quora_backup.py
+++ quorabackup/quora_backup.py
@@ -88,42 +88,42 @@ class QuoraBackup():
             if type == 'answers':
                 json_backup(self.activity.answers, os.path.join(path, 'answers.json'))
             elif type == 'questions':
-                json_backup(self.activity.questions, os.path.join(path, 'questions.json'))
+                json_backup(self.activity.want_answers, os.path.join(path, 'questions.json'))
             elif type == 'upvotes':
                 json_backup(self.activity.upvotes, os.path.join(path, 'upvotes.json'))
-            elif type == 'question_follows':
-                json_backup(self.activity.question_follows, os.path.join(path, 'question_follows.json'))
+            # elif type == 'question_follows':
+            #     json_backup(self.activity.question_follows, os.path.join(path, 'question_follows.json'))
             elif type == 'review_requests':
                 json_backup(self.activity.review_requests, os.path.join(path, 'review_requests.json'))
-            # elif type == 'user_follows':
-            #     json_backup(self.activity.user_follows, os.path.join(path, 'user_follows.json'))
+            elif type == 'user_follows':
+                json_backup(self.activity.user_follows, os.path.join(path, 'user_follows.json'))
             else:
                 json_backup(self.activity.answers, os.path.join(path, 'answers.json'))
-                json_backup(self.activity.questions, os.path.join(path, 'questions.json'))
+                json_backup(self.activity.want_answers, os.path.join(path, 'questions.json'))
                 json_backup(self.activity.upvotes, os.path.join(path, 'upvotes.json'))
-                json_backup(self.activity.question_follows, os.path.join(path, 'question_follows.json'))
+                # json_backup(self.activity.question_follows, os.path.join(path, 'question_follows.json'))
                 json_backup(self.activity.review_requests, os.path.join(path, 'review_requests.json'))
-                # json_backup(self.activity.user_follows, os.path.join(path, 'user_follows.json'))
+                json_backup(self.activity.user_follows, os.path.join(path, 'user_follows.json'))
         elif format == 'csv':
             if type == 'answers':
                 csv_backup(self.activity.answers, os.path.join(path, 'answers.csv'))
             elif type == 'questions':
-                csv_backup(self.activity.questions, os.path.join(path, 'questions.csv'))
+                csv_backup(self.activity.want_answers, os.path.join(path, 'questions.csv'))
             elif type == 'upvotes':
                 csv_backup(self.activity.upvotes, os.path.join(path, 'upvotes.csv'))
-            elif type == 'question_follows':
-                csv_backup(self.activity.question_follows, os.path.join(path, 'question_follows.csv'))
+            # elif type == 'question_follows':
+            #     csv_backup(self.activity.question_follows, os.path.join(path, 'question_follows.csv'))
             elif type == 'review_requests':
                 csv_backup(self.activity.review_requests, os.path.join(path, 'review_requests.csv'))
-            # elif type == 'user_follows':
-            #     csv_backup(self.activity.user_follows, os.path.join(path, 'user_follows.csv'))
+            elif type == 'user_follows':
+                csv_backup(self.activity.user_follows, os.path.join(path, 'user_follows.csv'))
             else:
                 csv_backup(self.activity.answers, os.path.join(path, 'answers.csv'))
-                csv_backup(self.activity.questions, os.path.join(path, 'questions.csv'))
+                csv_backup(self.activity.want_answers, os.path.join(path, 'questions.csv'))
                 csv_backup(self.activity.upvotes, os.path.join(path, 'upvotes.csv'))
-                csv_backup(self.activity.question_follows, os.path.join(path, 'question_follows.csv'))
+                # csv_backup(self.activity.question_follows, os.path.join(path, 'question_follows.csv'))
                 csv_backup(self.activity.review_requests, os.path.join(path, 'review_requests.csv'))
-                # csv_backup(self.activity.user_follows, os.path.join(path, 'user_follows.csv'))
+                csv_backup(self.activity.user_follows, os.path.join(path, 'user_follows.csv'))
         elif format == 'mongodb':
             from pymongo import MongoClient
             if mongodb_uri is not None:
@@ -135,21 +135,21 @@ class QuoraBackup():
             if type == 'answers':
                 mongodb_backup(self.activity.answers, collection, self.user, 'answer')
             elif type == 'questions':
-                mongodb_backup(self.activity.questions, collection, self.user, 'question')
+                mongodb_backup(self.activity.want_answers, collection, self.user, 'question')
             elif type == 'upvotes':
                 mongodb_backup(self.activity.upvotes, collection, self.user, 'upvote')
-            elif type == 'question_follows':
-                mongodb_backup(self.activity.question_follows, collection, self.user, 'question_follow')
+            # elif type == 'question_follows':
+            #     mongodb_backup(self.activity.question_follows, collection, self.user, 'question_follow')
             elif type == 'review_requests':
                 mongodb_backup(self.activity.review_requests, collection, self.user, 'review_request')
-            # elif type == 'user_follows':
-            #     mongodb_backup(self.activity.user_follows, collection, self.user, 'user_follow')
+            elif type == 'user_follows':
+                mongodb_backup(self.activity.user_follows, collection, self.user, 'user_follow')
             else:
                 mongodb_backup(self.activity.answers, collection, self.user, 'answer')
-                mongodb_backup(self.activity.questions, collection, self.user, 'question')
+                mongodb_backup(self.activity.want_answers, collection, self.user, 'question')
                 mongodb_backup(self.activity.upvotes, collection, self.user, 'upvote')
-                mongodb_backup(self.activity.question_follows, collection, self.user, 'question_follow')
+                # mongodb_backup(self.activity.question_follows, collection, self.user, 'question_follow')
                 mongodb_backup(self.activity.review_requests, collection, self.user, 'review_request')
-                # mongodb_backup(self.activity.user_follows, collection, self.user, 'user_follow')
+                mongodb_backup(self.activity.user_follows, collection, self.user, 'user_follow')
         else:
             print 'Backup format has not yet been implemented.'
