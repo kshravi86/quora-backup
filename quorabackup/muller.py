427ac1dcc28fea4b91438066c632e5228c848834
kshravi86 <kshravi86@gmail.com>
2017-03-09 22:16:40 +0530

    hook

--- quorabackup/muller.py
+++ quorabackup/muller.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 
 driver = webdriver.PhantomJS()
-driver.get("https://www.quora.com/Do-Indians-realize-what-they-have-done-to-Quora/answer/Deepak-Mehta-2")
+driver.get("https://www.quora.com/What-is-the-best-way-to-decline-to-tell-someone-your-salary-in-India/answer/Deepak-Mehta-2")
 ele=driver.find_elements_by_class_name('qtext_para')
 ans="<html><body>"
 for each in ele:
