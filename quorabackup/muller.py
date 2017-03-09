f3b2519dce6046a2b9367520e42dc2089fbcc6a6
kshravi86 <kshravi86@gmail.com>
2017-03-09 22:55:21 +0530

    hook

--- quorabackup/muller.py
+++ quorabackup/muller.py
@@ -5,6 +5,10 @@ from selenium import webdriver
 
 
 driver = webdriver.PhantomJS()
+  
+  
+ 
+ 
 driver.get("https://www.quora.com/What-is-the-best-way-to-decline-to-tell-someone-your-salary-in-India/answer/Deepak-Mehta-2")
 ele=driver.find_elements_by_class_name('qtext_para')
 ans="<html><body>"
--- quorabackup/newwwwwwwwwwwwwww.py
+++ quorabackup/newwwwwwwwwwwwwww.py
@@ -19,14 +19,22 @@ while True:
     elements = driver.find_elements_by_class_name('answer_permalink')
     print 'size' + str(len(elements))
     w = []
+    ans=""
     for each in elements:
-        #print each.text
+        
         if len(str(each.get_attribute('href'))) > 6:
-            #print str(each.get_attribute('href'))
+            
             driver3.get(str(each.get_attribute('href')))
             if float(driver3.find_element_by_class_name('meta_num').text[:len(driver3.find_element_by_class_name('meta_num').text) - 1]) > 100.0:
                print str(each.get_attribute('href'))
-               print driver3.find_element_by_class_name('meta_num').text[
-                  :len(driver3.find_element_by_class_name('meta_num').text) - 1]
-
+               yourstring=str(each.get_attribute('href'))
+               yourstring = yourstring.encode('ascii', 'ignore').decode('ascii')
+               ans=ans+yourstring
+   
     time.sleep(3)
+    f=open('subbu.txt','w')
+    f.write(ans)
+   
+    
+    
+    
