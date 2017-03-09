a7cfe447614564db58218cfbb940f3425dbba223
kshravi86 <kshravi86@gmail.com>
2017-03-09 22:07:23 +0530

    Hi

--- quorabackup/muller.py
+++ quorabackup/muller.py
@@ -7,13 +7,15 @@ from selenium import webdriver
 driver = webdriver.PhantomJS()
 driver.get("https://www.quora.com/Do-Indians-realize-what-they-have-done-to-Quora/answer/Deepak-Mehta-2")
 ele=driver.find_elements_by_class_name('qtext_para')
-ans=""
+ans="<html><body>"
 for each in ele:
    print each.text
    
    yourstring=each.text
    yourstring = yourstring.encode('ascii', 'ignore').decode('ascii')
-   ans=ans+yourstring
+   ans=ans+<br>+yourstring
+   
+ans=ans+"</body></html>"
 f=open('subbu.txt','w')
 f.write(ans)
    
