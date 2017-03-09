b1473d421e66ec32c4acb8e3e55b8db37fee3c89
kshravi86 <kshravi86@gmail.com>
2017-03-09 21:02:37 +0530

    go

--- quorabackup/muller.py
+++ quorabackup/muller.py
@@ -8,4 +8,4 @@ driver = webdriver.PhantomJS()
 driver.get("https://www.quora.com/Do-Indians-realize-what-they-have-done-to-Quora/answer/Deepak-Mehta-2")
 ele=driver.find_elements_by_class_name('qtext_para')
 for each in ele:
-  print str(each)
+  print str(each.text)
