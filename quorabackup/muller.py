f0ef79f375f95228d118142b73f670d23293c181
kshravi86 <kshravi86@gmail.com>
2017-03-11 10:14:24 +0530

    by

--- quorabackup/muller.py
+++ quorabackup/muller.py
@@ -9,7 +9,7 @@ driver = webdriver.PhantomJS()
   
  
  
-driver.get("https://www.quora.com/What-is-the-best-way-to-decline-to-tell-someone-your-salary-in-India/answer/Deepak-Mehta-2")
+driver.get("https://www.quora.com/What-are-some-study-hacks-every-student-should-know/answer/Deepak-Mehta-2")
 ele=driver.find_elements_by_class_name('qtext_para')
 ans="<html><body>"
 for each in ele:
