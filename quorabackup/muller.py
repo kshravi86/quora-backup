f5a3607fa4bdde744f7d491b7106c816afe43f3e
kshravi86 <kshravi86@gmail.com>
2017-03-11 10:24:53 +0530

    hi

--- quorabackup/muller.py
+++ quorabackup/muller.py
@@ -9,7 +9,7 @@ driver = webdriver.PhantomJS()
   
  
  
-driver.get("https://www.quora.com/What-are-some-study-hacks-every-student-should-know/answer/Deepak-Mehta-2")
+driver.get("https://www.quora.com/How-will-scrapping-the-INR-500-and-INR-1000-currency-notes-affect-the-daily-lives-of-Indians/answer/Deepak-Mehta-2")
 ele=driver.find_elements_by_class_name('qtext_para')
 ans="<html><body>"
 for each in ele:
