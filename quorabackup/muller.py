6a1b9f120dafa33b961474212109072d416fb9e4
kshravi86 <kshravi86@gmail.com>
2017-03-11 10:29:57 +0530

    UConn

--- quorabackup/muller.py
+++ quorabackup/muller.py
@@ -10,6 +10,8 @@ driver = webdriver.PhantomJS()
  
  
 driver.get("https://www.quora.com/How-will-scrapping-the-INR-500-and-INR-1000-currency-notes-affect-the-daily-lives-of-Indians/answer/Deepak-Mehta-2")
+
+
 ele=driver.find_elements_by_class_name('qtext_para')
 ans="<html><body>"
 for each in ele:
