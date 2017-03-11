63e3129bc58c17580c38e2531c05272be528c648
kshravi86 <kshravi86@gmail.com>
2017-03-11 23:49:39 +0530

    Create balajiabove6k.py

---
+++ balajiabove6k.py
@@ -0,0 +1,24 @@
+url  https://www.quora.com/Whats-the-reason-behind-the-Akhilesh-Yadav%E2%80%99s-loss-in-UP/answer/Balaji-Viswanathan-2
+6801 Upvoters
+url  https://www.quora.com/Of-all-the-Indian-Prime-Ministers-youve-seen-in-your-life-whom-do-you-respect-the-most-and-why/answer/Balaji-Viswanathan-2
+9809 Upvoters
+url  https://www.quora.com/Why-India-was-captured-and-ruled-by-many-Example-British-Muslim-rule-Others-like-French-Dutch-Portuguese-Are-Indians-so-weak/answer/Balaji-
+Viswanathan-2
+11023 Upvoters
+url  https://www.quora.com/What-do-you-think-about-recent-Ramjas-College-Violence-incident/answer/Balaji-Viswanathan-2
+11024 Upvoters
+url  https://www.quora.com/How-will-our-world-be-today-if-the-20th-century-didnt-witness-the-2-world-wars/answer/Balaji-Viswanathan-2
+6486 Upvoters
+url  https://www.quora.com/If-Ancient-Indian-military-was-so-sophisticated-then-how-did-they-lose-to-Arab-invaders/answer/Balaji-Viswanathan-2
+6806 Upvoters
+url  https://www.quora.com/Do-Microsoft-employees-use-Google-at-the-office/answer/Balaji-Viswanathan-2
+8190 Upvoters
+url  https://www.quora.com/If-Indians-are-thrown-out-of-the-USA-what-would-be-the-condition-of-the-USA-afterwards/answer/Balaji-Viswanathan-2
+13785 Upvoters
+url  https://www.quora.com/Is-it-civil-and-does-it-meet-the-parliamentary-ethics-of-PM-Narendra-Modis-comment-on-Manmohan-Singh-that-he-wears-a-raincoat-while-taking-
+a-bath/answer/Balaji-Viswanathan-2
+12660 Upvoters
+url  https://www.quora.com/What-are-your-views-on-the-Union-Budget-of-India-2017-18/answer/Balaji-Viswanathan-2
+7018 Upvoters
+url  https://www.quora.com/How-does-Donald-Trumps-ideology-compare-to-Narendra-Modis/answer/Balaji-Viswanathan-2
+7109 Upvoters
