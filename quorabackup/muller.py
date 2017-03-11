eab49f4999139828d0ea1bd25a99126ac40dc916
kshravi86 <kshravi86@gmail.com>
2017-03-11 10:34:43 +0530

    bin

--- quorabackup/muller.py
+++ quorabackup/muller.py
@@ -9,8 +9,7 @@ driver = webdriver.PhantomJS()
   
  
  
-driver.get("https://www.quora.com/How-will-scrapping-the-INR-500-and-INR-1000-currency-notes-affect-the-daily-lives-of-Indians/answer/Deepak-Mehta-2")
-
+driver.get("https://www.quora.com/How-would-demonetizing-500-and-1000-rupee-notes-and-introducing-new-2000-rupee-notes-help-curb-black-money-and-corruption/answer/Deepak-Mehta-2")
 
 ele=driver.find_elements_by_class_name('qtext_para')
 ans="<html><body>"
